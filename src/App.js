3d22f57bacfbbdface654c4f9ebc0b9cdada87b8
Gennaro Esposito <espoge@gmail.com>
2020-05-12 13:05:38 +0200

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-//import Tasks from './components/Tasks'
-import TasksReducer from './components/TasksReducer'
+import Tasks from './components/Tasks'
+//import TasksReducer from './components/TasksReducer'
 
 import './App.css';
 
 const App = props => {
-  //return <Tasks />
-  return <TasksReducer />
+  return <Tasks />
+  //return <TasksReducer />
 }
 
 export default App;
